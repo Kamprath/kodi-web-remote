ee4a89da6162819048d5c5f487c4e01202e08912
Johnny Kamprath <johnny.kamprath@archrival.com>
2015-03-27 15:13:44 -0500

    Add cache breaking to AppCache file via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
+var replace = require('gulp-replace');
 
 // Concat and minify JS
 gulp.task('build-js', function() {
@@ -26,8 +27,15 @@ gulp.task('build-css', function() {
         .pipe(gulp.dest('./public/css/'));
 });
 
+// Update AppCache file with random int to force browsers to re-download it
+gulp.task('update-appcache', function() {
+    gulp.src('./public/osmc_remote.appcache')
+        .pipe(replace(/cachebreaker:.*/, 'cachebreaker:' + Math.random().toString().substr(2)))
+        .pipe(gulp.dest('./public'));
+});
+
 // Set up default task and watches
-gulp.task('default', ['build-js', 'build-css'], function() {
-    gulp.watch('./src/js/*.js', ['build-js']);
-    gulp.watch('./src/less/*.less', ['build-css']);
+gulp.task('default', ['build-js', 'build-css', 'update-appcache'], function() {
+    gulp.watch('./src/js/*.js', ['build-js', 'update-appcache']);
+    gulp.watch('./src/less/*.less', ['build-css', 'update-appcache']);
 });
