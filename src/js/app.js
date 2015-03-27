ddabdc2ee546ca787edb480ecb78f80d59adcaf9
Johnny Kamprath <johnny.kamprath@archrival.com>
2015-03-27 15:19:14 -0500

    Improve app structure and interface
    * Created error screen for failed API calls
    * Documented methods
    * Added functionality to synchronize button states with server (play, stop, etc.)
    * Improved fullscreen switching

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,86 @@
 var App = App || {};
 
 App = {
-    serverUrl: '/jsonrpc',
+    serverUri: '/jsonrpc',
     isFullScreen: false,
 
+    /**
+     * Media center properties
+     */
+    properties: {
+        playing: null,
+        volume: null
+    },
+
+    /**
+     * jQuery selectors
+     */
     selectors: {
         button: '.btn',
+        interface: '.interface',
+        error: '.error',
+        errorMsg: '.error-message',
+        errorAction: '.error-action a',
+        hidden: 'hidden',
         navigateButtons: '.btn-navigate',
         playbackButtons: '.btn-playback',
         playbackButton: '.btn-playpause',
         menuButtons: '.btn-menu'
     },
 
-    properties: {
-        volume: null,
-        playing: null
-    },
-
+    /**
+     * Initialize the application
+     */
     init: function() {
         this.initEvents();
-        this.getProperties();
+        this.syncInterface();
     },
 
-    getProperties: function() {
+    /**
+     * Retrieve various server properties
+     * @param {function} cb     A callback function
+     */
+    getProperties: function(cb) {
         var self = this;
 
+        // Determine if anything is actively playing
         this.callApiMethod('Player.GetActivePlayers', null, function(data) {
             self.properties.playing = (data.result.length > 0);
 
-            if (self.properties.playing) {
-                self.togglePlaybackButton();
-            }
-        });
+            // Get volume
+            this.callApiMethod('Application.GetProperties', {'properties': ['volume']}, function(data) {
+                self.properties.volume = data.result.volume;
 
-        this.callApiMethod('Application.GetProperties', {'properties': ['volume']}, function(data) {
-            self.properties.volume = data.result.volume;
+                // Execute callback if set
+                if (typeof cb !== 'undefined' && cb !== null) cb.bind(self)();
+            });
         });
     },
 
+    /**
+     * Synchronize interface button states with server
+     */
+    syncInterface: function() {
+        var self = this;
+
+        this.getProperties(function() {
+
+            // set playback button status
+            if (self.properties.playing) {
+                $(self.selectors.playbackButton).removeClass('play');
+                $(self.selectors.playbackButton).addClass('pause');
+            } else {
+                $(self.selectors.playbackButton).removeClass('pause');
+                $(self.selectors.playbackButton).addClass('play');
+            }
+
+            // todo: set volume bar
+        });
+    },
+
+    /**
+     * Initialize event handlers
+     */
     initEvents: function() {
         var self = this;
 
@@ -45,17 +88,32 @@ App = {
         $(this.selectors.navigateButtons + ', ' + this.selectors.menuButtons).on('click', self.navigate.bind(self));
         $(this.selectors.playbackButtons).on('click', self.controlPlayback.bind(self));
 
-        // control fullscreen
-        $(this.selectors.button).on('click.fullscreenEvent', self.useFullScreen.bind(self));
-        $(window).on('blur', self.disableFullscreen.bind(self));
+        // toggle fullscreen on button push
+        $(this.selectors.button).on('click', self.useFullScreen.bind(self));
 
-        $(window).on('focus', self.getProperties.bind(self));
+        // sync interface when changing volume or playback
+        $(this.selectors.playbackButtons).on('click', self.syncInterface.bind(self));
+
+        // Close error when 'OK' is clicked
+        $(this.selectors.errorAction).on('click', function() { self.toggleError(false); return false; });
+
+        // set fullscreen status on fullscreen state change
+        $(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e) {
+            self.setFullscreenStatus(!self.isFullScreen);
+        });
     },
 
-    disableFullscreen: function() {
-        this.isFullScreen = false;
+    /**
+     * Set fullscreen status
+     * @param {bool} status
+     */
+    setFullscreenStatus: function(status) {
+        this.isFullScreen = status;
     },
 
+    /**
+     * Activate fullscreen
+     */
     useFullScreen: function() {
         if (!this.isFullScreen) {
             var doc = window.document;
@@ -66,8 +124,6 @@ App = {
 
             if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
                 requestFullScreen.call(docEl);
-                this.isFullScreen = true;
-                //$(this.selectors.button).unbind('click.fullscreenEvent');
             }
             else {
                 cancelFullScreen.call(doc);
@@ -75,7 +131,15 @@ App = {
         }
     },
 
+    /**
+     * Make an API call to the server using JSON-RPC
+     * @param {string} method       An API method
+     * @param {Object} params       (Optional) An object of parameters
+     * @param {function} cb         (Optional) A callback function that will receive a JSON object of response data
+     */
     callApiMethod: function(method, params, cb) {
+        var self = this;
+
         var data = {
             id: 1,
             jsonrpc: "2.0",
@@ -85,33 +149,66 @@ App = {
         if (typeof params !== 'undefined' && params !== null) data.params = params;
 
         var options = {
-            url: this.serverUrl,
+            url: this.serverUri,
             type: 'POST',
             dataType: 'json',
             contentType: 'application/json; charset=UTF-8',
+            error: self.displayError.bind(self),
+            success: function(data) {
+                self.toggleError(false);
+                if (typeof cb !== 'undefined' && cb !== null) cb.bind(self)();
+            },
             data: JSON.stringify(data)
         };
 
-        if (typeof cb !== 'undefined' && cb !== null) options.success = cb;
 
         $.ajax(options);
     },
 
+    /**
+     * Toggle visibility of error overlay
+     * @param {bool} isVisible
+     */
+    toggleError: function(isVisible) {
+        var $interface = $(this.selectors.interface);
+        var $error = $(this.selectors.error);
+
+        if (isVisible) {
+            $interface.addClass(this.selectors.hidden);
+            $error.removeClass(this.selectors.hidden);
+        } else {
+            $interface.removeClass(this.selectors.hidden);
+            $error.addClass(this.selectors.hidden);
+        }
+    },
+
+    /**
+     * Display an error message as the result of a failed AJAX request
+     * @param xhr
+     * @param status
+     */
+    displayError: function(xhr, status) {
+        $(this.selectors.errorMsg).text('Unable to reach the server.');
+        this.toggleError(true);
+    },
+
+    /**
+     * Call API method when navigational buttons are clicked
+     * @param e
+     * @returns {boolean}   Returns false
+     */
     navigate: function(e) {
         this.callApiMethod($(e.target).data('method'));
         return false;
     },
+
+    /**
+     * Call API method when playback buttons are clicked
+     * @param e
+     * @returns {boolean}   Returns false
+     */
     controlPlayback: function(e) {
-        var method = $(e.target).data('method');
-
-        if (method === 'Player.PlayPause') this.togglePlaybackButton();
-        this.callApiMethod(method, {playerid: 0});
-
+        this.callApiMethod($(e.target).data('method'), {playerid: 0});
         return false;
-    },
-
-    togglePlaybackButton: function() {
-        $(this.selectors.playbackButton).toggleClass('play');
-        $(this.selectors.playbackButton).toggleClass('pause');
     }
 };
